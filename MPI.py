fc2cfc1f4b69e9ffd3580611808b4a367adc68d0
? <alberto.garcia.2016@upb.edu.co>
2020-03-12 23:30:03 -0500

    Update MPI.py

--- MPI.py
+++ MPI.py
@@ -1,3 +1,4 @@
+from mpi4py import MPI
 global area, dx
 area=0
 def f(x):
@@ -12,16 +13,37 @@ def getArea(li):
 def getH(li):
     return f(li+dx/2)
     
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+nodes=comm.size()
+
+#Start nodes in sequential order
+myNode=nodes-1
+#print(comm, rank)
 
 #%Const
-#n=100000000
-n=100000000
+n=1000000
+#n=10000000000
 a=-5000
 b=5000
 dx=(b-a)/n
 
 
 #n= n/nodos*miNodo,n/nodos*(miNodo+1)       ->[miNodo] starts with 0
-for i in range(n):
+start=int(n/comm.size*myNode)
+stop=int(n/comm.size*(myNode+1))+2 if(myNode==comm.size()-1) else int(n/comm.size*(myNode+1))
+for i in range(start,stop):
     sumArea(getArea(iterationLi(i)))
-print(area)
+#print(area)
+
+if rank == 0:
+    data1 = 0.0
+    comm.Recv(data1, source=1, tag=1)
+    sumArea(data1)
+    data2 = 0.0
+    comm.Recv(data2, source=1, tag=2)
+    sumArea(data2)
+    
+elif rank == 1:
+    data = area
+    comm.Send(data, dest=0, tag=myNode)
